0750b75bda2465c2210d9800bdfc581e4fba9e1e
Rohit Prajapati <128039267+Rohit8022@users.noreply.github.com>
2025-09-26 18:12:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,5 +1,11 @@
 class Solution:
+    
     def rotate(self, nums: List[int], k: int) -> None:
+        def reverse(left,right):
+            while left<right:
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
+                right-=1
         """
         Do not return anything, modify nums in-place instead."""
         #Brutforce
@@ -11,5 +17,16 @@ class Solution:
                 nums[i]=nums[i-1]
             nums[0]=l
             r-=1  """ 
+        # WORK ONLY IN PYTHON
+        """    
         r=k%len(nums)    
-        nums[:]=nums= nums[len(nums)-r:] + nums[:len(nums)-r]     
+        nums[:]=nums= nums[len(nums)-r:] + nums[:len(nums)-r] """
+        # INTERVIEW PREFFERD SOLUTION(SAME FOR ALL LANGUAGE) 
+        l=len(nums)
+        reverse(l-k,l-1)
+        reverse(0,l-k-1)
+        reverse(0,l-1)
+        
+
+
+
