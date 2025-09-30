a032650229b547ff1e5445d75b6dda3026b89af4
Rohit Prajapati <128039267+Rohit8022@users.noreply.github.com>
2025-09-30 17:40:59 +0530

    Time: 4 ms (46.16%), Space: 18.9 MB (17.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,26 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        #TIME COMPLEXITY = O(n log n)
+        """
         nums.sort()
         for i in range(len(nums)):
             if i!=nums[i]:
                 return i
                 
-        return len(nums)         
+        return len(nums)
+        l=0
+        while l<len(nums)"""
+        #TIME COMPLEXITY = O(n) 
+        #SPACE COMPLEXITY = O(n)
+        freq=[]
+        for i in range(len(nums)+1):
+            freq.append(-1) 
+        for i in range(len(nums)):
+            freq[nums[i]]=nums[i]
+        for i in range(len(freq)):
+            if freq[i]==-1:
+                return i    
+
+
+                      
         
