06c0d4c33f55c014dc662edca3ad287d57066367
Rohit Prajapati <128039267+Rohit8022@users.noreply.github.com>
2025-10-02 17:33:14 +0530

    Time: 50 ms (49.49%), Space: 42.2 MB (41.14%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,9 +1,11 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
+        #MY APPROCH
+        '''
         l=0
         r=0
         op=list()
-        ch_l=1
+        ch_l=1#CHECK FLAG 1=WILL RUN/0=WILL NOT
         while r<len(nums):
             if ch_l==1 and l<len(nums):
                 if nums[l]>0:
@@ -14,8 +16,21 @@ class Solution:
                 if nums[r]<0: 
                     op.append(nums[r])
                     ch_l=1
-                r+=1
-        #if len(nums)!=len(op):op.append(nums[r])        
-        return op           
+                r+=1       
+        return op
+        '''
+        #CODE AND DEBUG
+        op=[0]*len(nums)
+        pos=0
+        neg=1
+        for i in range(len(nums)):
+            if nums[i]>0:
+                op[pos]=nums[i]
+                pos+=2
+            else:
+                op[neg]=nums[i]
+                neg+=2
+        return op            
+
 
         
